8945a8fab7d27feb8a243f318b9c07e89ac823fe
Alexander Breen <alexander.breen@gmail.com>
2015-08-26 13:47:11 -0400

    Added test controls defaults and update functionality

--- src/main/java/io/breen/socrates/view/main/TestControls.java
+++ src/main/java/io/breen/socrates/view/main/TestControls.java
@@ -1,19 +1,23 @@
 package io.breen.socrates.view.main;
 
 import io.breen.socrates.Globals;
+import io.breen.socrates.immutable.test.Automatable;
+import io.breen.socrates.immutable.test.Test;
+import io.breen.socrates.model.TestNode;
 
 import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
 import java.awt.*;
+import java.text.DecimalFormat;
 
 public class TestControls {
 
     private enum TestProperty {
-        TEST_TYPE(0, "Test type", "Unknown"),
-        DEDUCTION(1, "Deduction", "0 points"),
-        IS_AUTOMATED(2, "Automated", "Unknown");
+        TEST_TYPE(0, "Test type", "—"),
+        DEDUCTION(1, "Deduction", "—"),
+        IS_AUTOMATED(2, "Automated", "—");
 
         public final int index;
         public final String labelText;
@@ -79,5 +83,29 @@ public class TestControls {
                 TestProperty.getKeys(), TestProperty.getDefaults()
         );
         properties.setOpaque(false);
+    }
+
+    public void update(TestNode testNode) {
+        Test test = testNode.test;
+        DecimalFormat fmt = new DecimalFormat("#.#");
+
+        description.setText(test.description);
+
+        properties.set(
+                TestProperty.TEST_TYPE.index,
+                test.getClass().getSimpleName()
+        );
+
+        properties.set(
+                TestProperty.DEDUCTION.index,
+                test.deduction == 1.0 ? "1 point" : fmt.format(test.deduction) + " points"
+        );
+
+        properties.set(
+                TestProperty.IS_AUTOMATED.index,
+                test instanceof Automatable ? "Yes" : "No"
+        );
+
+        notes.setDocument(testNode.getNotesDocument());
     }
 }
