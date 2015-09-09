ee59d6e09fedac5338a00e51d0b07bc0b537157f
Alexander Breen <alexander.breen@gmail.com>
2015-09-09 12:00:50 -0400

    Fixed bug with equality on lists/arrays

--- src/main/java/io/breen/socrates/immutable/test/implementation/python/VariableEvalTest.java
+++ src/main/java/io/breen/socrates/immutable/test/implementation/python/VariableEvalTest.java
@@ -8,6 +8,8 @@ import io.breen.socrates.immutable.test.*;
 import org.apache.xmlrpc.XmlRpcException;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 public class VariableEvalTest extends VariableTest implements Automatable<PythonFile> {
 
@@ -25,6 +27,49 @@ public class VariableEvalTest extends VariableTest implements Automatable<Python
         super(deduction, description);
     }
 
+    public static boolean equals(Object expected, Object other) throws CannotBeAutomatedException {
+        if (expected == null) {
+            return other == null;
+
+        } else if (expected instanceof Integer) {
+            Integer n = (Integer)expected;
+            if (other == null || !(other instanceof Integer)) return false;
+            return n.equals(other);
+
+        } else if (expected instanceof Double) {
+            Double n = (Double)expected;
+            if (other == null || !(other instanceof Double)) return false;
+            return n.equals(other);
+
+        } else if (expected instanceof String) {
+            String s = (String)expected;
+            if (other == null || !(other instanceof String)) return false;
+            return s.equals(other);
+
+        } else if (expected instanceof List) {
+            List list = (List)expected;
+            if (other == null) return false;
+
+            if (other instanceof Object[]) {
+                Object[] objArr = (Object[])other;
+                for (int i = 0; i < objArr.length; i++) {
+                    if (!list.get(i).equals(objArr[i])) return false;
+                }
+                return true;
+            }
+
+            return list.equals(other);
+
+        } else if (expected instanceof Map) {
+            Map map = (Map)expected;
+            if (other == null || !(other instanceof Map)) return false;
+            return map.equals(other);
+
+        }
+
+        throw new CannotBeAutomatedException();
+    }
+
     @Override
     public String getTestTypeName() {
         return "Python evaluation test";
@@ -40,7 +85,7 @@ public class VariableEvalTest extends VariableTest implements Automatable<Python
         try (PythonInspector inspector = new PythonInspector(target.fullPath)) {
             inspector.openModule(parent.getModuleName());
             Object value = inspector.variableEval(var.name);
-            return this.value.equals(value);
+            return equals(this.value, value);
 
         } catch (IOException | XmlRpcException x) {
             throw new AutomationFailureException(x);
