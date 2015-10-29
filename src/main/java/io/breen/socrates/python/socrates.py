8b1f25cfd4cb50763d0d54f7fbb48f756c3abcd5
Alexander Breen <alexander.breen@gmail.com>
2015-10-28 23:51:19 -0400

    Updated the Python API for custom tests

--- src/main/java/io/breen/socrates/python/socrates.py
+++ src/main/java/io/breen/socrates/python/socrates.py
@@ -1,14 +1,32 @@
+"""
+This module allows a Python script to provide a custom implementation of a test for Socrates.
+Any script can be used as a Socrates test, so long as it imports this module. In all cases, scripts
+must end with a call to one of the test conclusion functions (see source code). Scripts must not
+rely on taking input from the standard in.
+
+To obtain any parameters to the test specified in the criteria file, scripts may use the
+get_test_parameters() function.
+
+Note: when this module is imported, it waits for JSON-encoded input from Socrates. Therefore,
+this module should be imported immediately (i.e., do not import this module inside a function).
+"""
+
 import sys
+
+import json
 
 EXIT_PASSED = 1
 EXIT_FAILED = 2
 
+# wait for Socrates to supply the parameters to the test
+params = json.loads(input())
+
+
+def get_parameters():
+    return params
 
 def pass_test():
-    print('passed')
     sys.exit(EXIT_PASSED)
 
-
 def fail_test():
-    print('failed')
     sys.exit(EXIT_FAILED)
