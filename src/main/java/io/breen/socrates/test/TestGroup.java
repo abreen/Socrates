c400f0737bde98cb00b6477abe5315b0318c5ac4
Alexander Breen <alexander.breen@gmail.com>
2016-02-02 21:35:05 -0500

    Updated documentation

--- src/main/java/io/breen/socrates/test/TestGroup.java
+++ src/main/java/io/breen/socrates/test/TestGroup.java
@@ -7,8 +7,7 @@ import java.util.List;
  * Represents a sequence of one or more "member" tests that run together. In addition, a TestGroup
  * represents two "ceilings": a maximum number of tests that can possibly fail in the group, and a
  * maximum number of points that can be taken by tests in the group that fail. Either, both, or none
- * of these ceilings may be specified. If a ceiling is not specified, it defaults to Ceiling.ANY
- * (which represents no limit).
+ * of these ceilings may be specified in a criteria file.
  *
  * Test groups are the only way to represent "decisions" in a criteria file. For example, it is
  * common to use a test group with a maxNum of 1 and two member tests. This means that, as soon as
@@ -17,7 +16,6 @@ import java.util.List;
  * lightweight way.
  *
  * @see io.breen.socrates.test.Test
- * @see io.breen.socrates.test.ceiling.Ceiling
  */
 public class TestGroup {
 
